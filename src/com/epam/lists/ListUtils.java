de18136adedd47b077a832df6e8aba693c04793e
Petr_Shypila <ikrumping@gmail.com>
2016-10-25 16:45:03 +0200

    Finished several LinkedList exercises

---
+++ src/com/epam/lists/ListUtils.java
@@ -0,0 +1,7 @@
+package com.epam.lists;
+
+public class ListUtils {
+    public static Node builtLinkedFromArray() {
+        return new Node(8, new Node(2, new Node(14, new Node(3, new Node(18, new Node(0, new Node(23, new Node(32, null))))))));
+    }
+}
